fc2f40a17046f64df025274b9e8baa8d6f553d2c
cmouli96 <cmoulignt@gmail.com>
2026-03-26 22:27:28 +0530

    Delete app directory

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Contact</h1>
-      <p>Email: founder@adf2fabric.com</p>
-      <p>Country: India</p>
-    </div>
-  );
-}
